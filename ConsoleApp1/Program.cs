85432a2df293ce226e4c970dee2b23b1864eee1a
Даниил <puiuld47@gmail.com>
2025-01-24 14:42:48 +0200

    Task 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task а)
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for (int j = 1; j < 11; j++)
+            //    Console.Write($"{j}\t");
+            //    Console.WriteLine();
+            //    Console.ReadLine();
+            //}
+
+            //Task б)
+            //for (int i = 4; i < 8; i++)
+            //{
+            //    for (int j = 1; j < 11; j++)
+            //    Console.Write($"{i*10+j}\t");
+            //    Console.WriteLine();
+            //    Console.ReadLine();
+            //}
+
+            //Task в)
+            //for(int i=1; i<=5; i++)
+            //{
+            //    for(int j=0; j<i; j++)
+            //    Console.Write("5\t");
+            //    Console.WriteLine();
+            //    Console.ReadLine();
+            //}
+
+            //Task г)
+            //for (int i = 5; i > 0; --i)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    Console.Write("1");
+            //    Console.WriteLine();
+            //    Console.ReadLine();
+            //}
+
+        }
+    }
+}
+
+
