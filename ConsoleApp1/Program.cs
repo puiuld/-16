501a55b2e51074e7439b85d3a64f83cf05c32fca
Даниил <puiuld47@gmail.com>
2025-01-24 15:20:45 +0200

    Task 4

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < i; j++) Console.Write(" ");
+                Console.Write("#");
+                for (int j = i; j < 8; j++) Console.Write(" ");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
